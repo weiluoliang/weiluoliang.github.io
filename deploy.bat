3ca70fe078719a84e47f81d9c7766523200089a3
luoliang <weiluoliang@yl-scm.com>
2022-04-26 18:37:20 +0800

    '更新主题'

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,13 @@
 call yarn build
 
-call cd docs/.vuepress/dist
+call cd docs\.vuepress\dist
 
-
-call echo 'blog.luoliang.top' > CNAME
+call echo blog.luoliang.top> CNAME
 
 call git init
 call git add -A
-call git commit -m "${msg}"
-call git push -f git@github.com:weiluoliang/weiluoliang.github.io.git master:gh-page # 推送到github gh-pages分支
+call git commit -m 'deploy'
 
-cd -
-rm -rf docs/.vuepress/dist
+call git push -f git@github.com:weiluoliang/weiluoliang.github.io.git master:gh-page
+
+call cd ..\..\..
