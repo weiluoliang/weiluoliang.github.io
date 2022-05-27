34c9d5d7d966243262c2ae7a99cc2f08952dfd26
luoliang <weiluoliang@yl-scm.com>
2022-05-27 18:31:52 +0800

    友情链接

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -39,6 +39,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: '归档', link: '/archives/' },
         ],
       },
+      {
+        text: '友情连接',
+        link: '/pages/6b2d2f/',
+      }
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     // logo: 'https://cdn.jsdelivr.net/gh/weiluoliang/img@main/blog/book.webp', // 导航栏logo
